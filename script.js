8cfa0c246857a1a68ffe9a0d561d63965d44a971
Nico Michel <nicomichel@utexas.edu>
2020-06-28 23:26:47 -0500

    changed code, now returns multiple characters but there is an HTML element bug as well as a character count issue

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 var lowerCaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 var upperCaseCharacters = ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'];
-var randomPasswordGenerated =""
+var allCharacters = "0123456789!@#$%^&*()abcdefghijKLMNOPQRST";
+var randomPasswordGenerated ="";
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -26,21 +27,25 @@ function generatePassword() {
   var numericChar = confirm("Do you want numeric characters?");
   var specialChar = confirm("Do you want special characters?");
 
-  if (lowerCase===true) {
-    return lowerCaseCharacters[Math.floor(Math.random()*lowerCaseCharacters.length)];
+  for (var i=0, n=allCharacters.length; i<128; i++){
+    password += allCharacters.charAt(Math.floor(Math.random()* n));
   }
+  return password
+  // if (lowerCase===true) {
+  //   return lowerCaseCharacters[Math.floor(Math.random()*lowerCaseCharacters.length)];
+  // }
 
-  if (upperCase===true) {
-    return upperCaseCharacters[Math.floor(Math.random()*upperCaseCharacters.length)];
-  }
+  // if (upperCase===true) {
+  //   return upperCaseCharacters[Math.floor(Math.random()*upperCaseCharacters.length)];
+  // }
 
-  if (numericChar===true) {
-    return numericCharacters[Math.floor(Math.random()*numericCharacters.length)];
-  }
+  // if (numericChar===true) {
+  //   return numericCharacters[Math.floor(Math.random()*numericCharacters.length)];
+  // }
 
-  if (specialChar===true) {
-    return specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
-  }
+  // if (specialChar===true) {
+  //   return specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+  // }
  
   
 
